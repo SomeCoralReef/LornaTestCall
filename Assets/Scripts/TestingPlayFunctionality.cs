e9348b1601938b5b189f860c1c88d511da27f3a7
Reef <deyingliew@gmail.com>
2023-12-08 11:47:18 -0600

    Auto stash before merge of "ReefBranch" and "origin/ReefBranch"
    Pushed 1 for Toning and 2 for start and stop silent

--- Assets/Scripts/TestingPlayFunctionality.cs
+++ Assets/Scripts/TestingPlayFunctionality.cs
@@ -6,6 +6,7 @@ public class TestingPlayFunctionality : MonoBehaviour
 {
     public AK.Wwise.Event TestCall_B_Toning;
     public AK.Wwise.Event TestCall_B_Silent;
+    private bool TestCall_B_SilentPlaying = false;
 
     // Update is called once per frame
     void Update()
@@ -13,10 +14,18 @@ public class TestingPlayFunctionality : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Alpha1)){
             Debug.Log("Playing TestCall_B_Toning");
             TestCall_B_Toning.Post(gameObject);
+            //TestCall_B_Silent.Stop(gameObject);
         }
         if(Input.GetKeyDown(KeyCode.Alpha2)){
-            Debug.Log("Playing TestCall_B_Silent");
-            TestCall_B_Silent.Post(gameObject);
+            if(TestCall_B_SilentPlaying){
+                Debug.Log("Stopping TestCall_B_Silent");
+                TestCall_B_Silent.Stop(gameObject);
+                TestCall_B_SilentPlaying = false;
+            } else {
+                Debug.Log("Playing TestCall_B_Silent");
+                TestCall_B_Silent.Post(gameObject);
+                TestCall_B_SilentPlaying = true;
+            }
         }
     }
 }
